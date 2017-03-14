b52ccd9ee8626693ef3ac1d63397f117940a70a3
Zach Marcolesco <zach.marcolesco@gmail.com>
2017-03-14 14:21:13 -0500

    Add more routing.

--- public/app.js
+++ public/app.js
@@ -8,5 +8,15 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
       console.log(xhr.responseText);
+      var projects = JSON.parse(xhr.responseText);
+      projects.forEach(function(project) {
+        var name = document.createElement('a');
+        name.innerHTML = project.name;
+        name.href = "/projects/" + project.id;
+        document.body.appendChild(name);
+      });
+    } else {
+      console.log('Error:' + xhr.status);
+    }
   }
 }
